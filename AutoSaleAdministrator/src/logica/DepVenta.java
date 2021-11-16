266420caafc75b985db3d87ce123f70aaaab7dbb
Santiago0310 <90292735+Santiago0310@users.noreply.github.com>
2021-11-16 13:33:15 -0500

    Add files via upload

---
+++ AutoSaleAdministrator/src/logica/DepVenta.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+import datos.Registro;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author 57321
+ */
+public class DepVenta {
+    
+    private ArrayList<Registro> registros;
+    private Long gananciasGeneradas;
+    
+    public DepVenta() {
+        if(this.registros == null){
+            this.registros = new ArrayList();
+        }
+    }
+
+    public void setRegistros(ArrayList<Registro> registros) {
+        this.registros = registros;
+    }
+    
+    
+    public Long getGananciasGeneradas() {
+        return gananciasGeneradas;
+    }
+
+    public void setGananciasGeneradas(Long gananciasGeneradas) {
+        this.gananciasGeneradas = gananciasGeneradas;
+    }
+    
+    public boolean agregarVenta(Registro registro){
+        if(registro == null){
+            return false;
+        }
+        if(this.registros.add(registro)){          
+            return true;
+        }
+        
+        return false;
+    }
+    public void mostrar(){
+        for(int i=0; i<this.registros.size(); i++){
+            System.out.println(registros.get(i).getCliente().getCedula());
+        }
+    }
+    public ArrayList<Registro> getRegistros() {
+        return registros;
+    }
+    
+    
+}
